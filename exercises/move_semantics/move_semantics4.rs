00ff979a928583a8ee09cf77dbf250c1a937b6d4
seven_bear <26707756+yueneiqi@users.noreply.github.com>
2025-08-23 23:12:05 +0800

    update

--- exercises/move_semantics/move_semantics4.rs
+++ exercises/move_semantics/move_semantics4.rs
@@ -7,12 +7,8 @@
 // Execute `rustlings hint move_semantics4` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let vec0 = Vec::new();
-
-    let mut vec1 = fill_vec(vec0);
+    let mut vec1 = create_vec();
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 
@@ -21,9 +17,9 @@ fn main() {
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-// `fill_vec()` no longer takes `vec: Vec<i32>` as argument
-fn fill_vec() -> Vec<i32> {
-    let mut vec = vec;
+// `create_vec()` no longer takes `vec: Vec<i32>` as argument
+fn create_vec() -> Vec<i32> {
+    let mut vec = Vec::new();
 
     vec.push(22);
     vec.push(44);
